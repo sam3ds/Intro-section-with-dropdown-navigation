4de6e5fab595f6af261214788ea45c42208b5e88
sam3ds <sam3ds@gmail.com>
2022-08-01 07:52:47 +0300

    edit login

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 let navbar = document.querySelector(".navbar");
+
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let closeOpenBtn = document.querySelector(".nav-links .bx-x");
+let navLinks = document.querySelector(".nav-links");
+
+menuOpenBtn.addEventListener("click", () => {
+    navLinks.style.right = "0";
+});
+
+closeOpenBtn.addEventListener("click", () => {
+    navLinks.style.right = "-100%";
+});
+
+// sidebar sub menu open close js code
+
+let featurescssArrow = document.querySelector(".featurescss-arrow");
+featurescssArrow.addEventListener("click", () => {
+    navLinks.classList.toggle("show1")
+});
+
+let companycssArrow = document.querySelector(".companycss-arrow");
+companycssArrow.addEventListener("click", () => {
+    navLinks.classList.toggle("show2")
+});
