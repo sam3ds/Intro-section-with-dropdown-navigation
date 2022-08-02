b96036d023bcbb958b344e5b4b135ee996000bef
sam3ds <sam3ds@gmail.com>
2022-08-02 08:33:03 +0300

    add overlay

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ let navbar = document.querySelector(".navbar");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let closeOpenBtn = document.querySelector(".nav-links .bx-x");
 let navLinks = document.querySelector(".nav-links");
+let overlay = document.querySelector(".overlay");
 
 menuOpenBtn.addEventListener("click", () => {
     navLinks.style.right = "0";
+    overlay.style.display = "block";
 });
 
 closeOpenBtn.addEventListener("click", () => {
     navLinks.style.right = "-100%";
+    overlay.style.display = "none";
 });
 
 // sidebar sub menu open close js code
