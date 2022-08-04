a95fcb91d2cb25dabb6a31a7d70a099dde9bf406
Hussain <you@example.com>
2022-08-04 15:45:52 +0300

    disable scrol when side-menu open

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let closeOpenBtn = document.querySelector(".nav-links .bx-x");
 let navLinks = document.querySelector(".nav-links");
 let overlay = document.querySelector(".overlay");
+let container = document.querySelector(".container");
 
 menuOpenBtn.addEventListener("click", () => {
     navLinks.style.right = "0";
     overlay.style.display = "block";
+    document.getElementsByTagName('html')[0].style.overflowY = "hidden";
 });
 
 closeOpenBtn.addEventListener("click", () => {
     navLinks.style.right = "-100%";
     overlay.style.display = "none";
+    document.getElementsByTagName('html')[0].style.overflowY = "auto";
+});
+
+container.addEventListener("click", () => {
+    navLinks.style.right = "-100%";
+    overlay.style.display = "none";
+    document.getElementsByTagName('html')[0].style.overflowY = "auto";
 });
 
 // sidebar sub menu open close js code
